691b3e2171a890269450c5f6083ce76199c711d9
cutright <dan.cutright@gmail.com>
2020-01-02 12:56:56 -0600

    global search for energy, allow for Gy or cGy units, track Beam Count, TypeError bug fixes

--- IQDM/parsers/delta4.py
+++ IQDM/parsers/delta4.py
@@ -5,8 +5,18 @@ Created on Thu May 30 2019
 @author: Dan Cutright, PhD
 """
 
-from ..utilities import are_all_strings_in_text, get_csv
+from utilities import are_all_strings_in_text, get_csv
 from dateutil.parser import parse as date_parser
+
+
+# So far I've only come across Composite and Fraction as beam name place holders for the composite row
+COMPOSITE_BEAM_NAMES = ['Composite', 'Fraction']
+
+# If you provide your possible energies here, script will do a global search instead of trying to parse the table
+# Table parsing was difficult, but this seems to work consistently.  For example, if '6 MV, FFF' is found anywhere
+# in the PDF, the energy will be assumed to be 6 MV, FFF and then stop looking through the other options, therefore,
+# the order of ENERGY_OPTIONS is important.  Set ENERGY_OPTIONS to None or [] to skip this feature
+ENERGY_OPTIONS = ['6 MV, FFF', '6 MV', '10 MV, FFF', '10 MV']
 
 
 class Delta4Report:
@@ -14,7 +24,7 @@ class Delta4Report:
         self.report_type = 'delta4'
         self.columns = ['Patient Name', 'Patient ID', 'Plan Date', 'Energy', 'Daily Corr', 'Norm Dose', 'Dev', 'DTA',
                         'Gamma-Index', 'Dose Dev', 'Radiation Dev', 'Gamma Pass Criteria', 'Gamma Dose Criteria',
-                        'Gamma Dist Criteria']
+                        'Gamma Dist Criteria', 'Beam Count']
         self.identifiers = ['ScandiDos AB', 'Treatment Summary', 'Acceptance Limits', 'Daily corr',
                             'Selected Detectors', 'Parameter Definitions & Acceptance Criteria, Detectors']
 
@@ -51,15 +61,18 @@ class Delta4Report:
             self.index_start['Beam'] = self.get_index_of_next_text_block(self.index_end['Beam'])
             self.index_end['Beam'] = self.get_index_of_next_text_block(self.index_start['Beam']) - 1
         self.data['Beam'] = self.get_data_block('Beam')
-        if 'Fraction' in self.data['Beam'][0]:
-            self.data['Beam'].pop(0)
+        for composite_name_option in COMPOSITE_BEAM_NAMES:
+            if composite_name_option in self.data['Beam'][0]:
+                self.data['Beam'].pop(0)
 
         # Gantry
         self.index_start['Gantry'] = self.get_index_of_next_text_block(self.index_end['Beam'])
         self.index_end['Gantry'] = self.get_index_of_next_text_block(self.index_start['Gantry']) - 1
         self.data['Gantry'] = ['N/A'] + self.get_data_block('Gantry')
-        if 'Fraction' in self.data['Gantry']:
-            self.data['Gantry'].pop(self.data['Gantry'].index('Fraction'))
+        for composite_name_option in COMPOSITE_BEAM_NAMES:
+            if composite_name_option in self.data['Gantry']:
+                self.data['Gantry'].pop(self.data['Gantry'].index(composite_name_option))
+
         energy_override = []  # sometimes the energy is on the same line as the gantry
         for i, row in enumerate(self.data['Gantry']):
             self.data['Gantry'][i] = row.replace('\xc2', '').replace('\xb0', '')
@@ -77,16 +90,18 @@ class Delta4Report:
         while analysis_data_block:
             row = analysis_data_block.pop(0)
 
-            if 'cGy' not in row:
+            # Sometime dose Norm Dose and other analysis data aren't in the same string,
+            # and sometimes if different order. Ensure they are in same string with Norm Dose first.
+            if 'Gy' not in row:
                 row = "%s %s" % (analysis_data_block.pop(0), row)
-
             if '%' not in row:
                 row = "%s %s" % (row, analysis_data_block.pop(0))
 
-            if 'cGy' in row and '%' in row:
+            if 'Gy' in row and '%' in row:
                 row = row.split('%')
-                data = [row[0].split('cGy')[0].strip(),
-                        row[0].split('cGy')[1].strip()]
+                split = ['Gy', 'cGy']['cGy' in row[0]]  # Report may be in cGy or Gy
+                data = [row[0].split(split)[0].strip(),
+                        row[0].split(split)[1].strip()]
                 data.extend(row[1:-1])
                 analysis_data.append(data)
 
@@ -95,6 +110,11 @@ class Delta4Report:
         self.data['DTA'] = [row[2].strip() for row in analysis_data]
         self.data['Gamma-Index'] = [row[3].strip() for row in analysis_data]
         self.data['Dose Dev'] = [row[4].strip() for row in analysis_data]
+
+        try:
+            self.data['Norm Dose'][0] = float(self.data['Norm Dose'][0])
+        except:
+            pass
 
         # Daily Correction Factor
         self.index_start['Daily Corr'] = self.get_index_of_next_text_block(self.index_end['Analysis'])
@@ -111,16 +131,22 @@ class Delta4Report:
                 self.data['Daily Corr'][i] = row[-5:]
 
         # Energy
-        self.index_start['Energy'] = self.get_index_of_next_text_block(self.index_end['Daily Corr'])
-        if 'dose dev' in self.text[self.index_start['Energy']]:
-            self.index_start['Energy'] = self.get_index_of_next_text_block(self.index_start['Energy'])
-        self.index_end['Energy'] = self.get_index_of_next_text_block(self.index_start['Energy']) - 1
-        self.data['Energy'] = ['N/A'] + self.get_data_block('Energy')
-        if any(energy_override):  # replace values with overrides found in Gantry code block
-            for i, override in enumerate(energy_override):
-                if override is not None:
-                    if len(self.data['Energy']) > i:
-                        self.data['Energy'][i] = override
+        self.data['Energy'] = None
+        if ENERGY_OPTIONS:
+            for energy_option in ENERGY_OPTIONS:
+                if self.data['Energy'] is None and energy_option in text_data:
+                    self.data['Energy'] = [energy_option.replace(',', '')] * len(energy_override)
+        if self.data['Energy'] is None:
+            self.index_start['Energy'] = self.get_index_of_next_text_block(self.index_end['Daily Corr'])
+            if 'dose dev' in self.text[self.index_start['Energy']]:
+                self.index_start['Energy'] = self.get_index_of_next_text_block(self.index_start['Energy'])
+            self.index_end['Energy'] = self.get_index_of_next_text_block(self.index_start['Energy']) - 1
+            self.data['Energy'] = ['N/A'] + self.get_data_block('Energy')
+            if any(energy_override):  # replace values with overrides found in Gantry code block
+                for i, override in enumerate(energy_override):
+                    if override is not None:
+                        if len(self.data['Energy']) > i:
+                            self.data['Energy'][i] = override
 
         # Gamma Criteria
         self.index_start['Gamma Criteria'] = self.text.index('Parameter Definitions & Acceptance Criteria, Detectors')
@@ -190,13 +216,19 @@ class Delta4Report:
 
     @property
     def summary_data(self):
+        try:
+            daily_corr = sum([float(f) for f in self.data['Daily Corr'] if f != 'N/A']) / (len(self.data['Daily Corr']) - 1)
+        except:
+            print('WARNING: Could not process daily corr for %s - %s' %
+                  (self.data['patient_name'], self.data['patient_id']))
+            daily_corr = 1.
+
         return {'Patient Name': self.data['patient_name'],
                 'Patient ID': self.data['patient_id'],
                 'Plan Date': self.measured_date,
                 'Energy': '/'.join(list(set([e for e in self.data['Energy'] if e != 'N/A']))),
-                'Daily Corr': sum([float(f) for f in self.data['Daily Corr'] if f != 'N/A']) / (
-                            len(self.data['Daily Corr']) - 1),
-                'Norm Dose': float(self.data['Norm Dose'][0]),
+                'Daily Corr': daily_corr,
+                'Norm Dose': self.data['Norm Dose'][0],
                 'Dev': float(self.data['Dev'][0]),
                 'DTA': float(self.data['DTA'][0]),
                 'Gamma-Index': float(self.data['Gamma-Index'][0]),
@@ -204,7 +236,8 @@ class Delta4Report:
                 'Radiation Dev': self.radiation_device,
                 'Gamma Pass Criteria': float(self.data['gamma_pass']),
                 'Gamma Dose Criteria': float(self.data['gamma_dose']),
-                'Gamma Dist Criteria': float(self.data['gamma_dist'])}
+                'Gamma Dist Criteria': float(self.data['gamma_dist']),
+                'Beam Count': len(self.data['Beam'])}
 
     @property
     def csv(self):
