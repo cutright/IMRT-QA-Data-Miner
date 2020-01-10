c74037069a67d77a23d509362f89b86a85c51495
cutright <dan.cutright@gmail.com>
2020-01-09 19:17:56 -0600

    0.3.1dev initial commit

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     name='IQDM',
     include_package_data=True,
     packages=find_packages(),
-    version='0.3',
+    version='0.3.1dev',
     description='Scans a directory for IMRT QA results',
     author='Dan Cutright',
     author_email='dan.cutright@gmail.com',
